fa3081173102514568b676dc9961cecb28036c2c
Jukalekvi <jukka.virolainen@myy.haaga-helia.fi>
2025-10-06 12:08:25 +0300

    src version check

--- src/submit_run.py
+++ src/submit_run.py
@@ -28,4 +28,4 @@ def submit_pipeline():
 if __name__ == "__main__":
     submit_pipeline()
 
-#This is a test which hopefully works now
+#This is a test which hopefully works now as well as in the notebook version
